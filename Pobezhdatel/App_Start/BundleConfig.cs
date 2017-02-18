97cdf218f2f07a49d33286ff8df2f9fa4ca33d5d
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-02-18 19:31:24 +0300

    Added a blank ASP.NET MVC project with SignalR.

---
+++ Pobezhdatel/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace Pobezhdatel
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Add scripts
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/JQuery/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/signalr").Include("~/Scripts/JQuerySignalR/jquery.signalR-{version}.js"));
+
+            // Add styles
+            bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/site.css"));
+        }
+    }
+}
---
+++ Pobezhdatel/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Pobezhdatel
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-Ru");
+        }
+    }
+}
