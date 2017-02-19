203fc8d24001bdbe806d8323512b957676044044
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-02-19 18:31:45 +0300

    Fixed a bug of bootstrap including.

--- Pobezhdatel/App_Start/BundleConfig.cs
+++ Pobezhdatel/App_Start/BundleConfig.cs
@@ -9,11 +9,11 @@ namespace Pobezhdatel
             // Add scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/JQuery/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/signalr").Include("~/Scripts/JQuerySignalR/jquery.signalR-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/Bootstrap/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap_scripts").Include("~/Scripts/Bootstrap/bootstrap.js"));
 
             // Add styles
             bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/Bootstrap/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap_styles").Include("~/Content/Bootstrap/bootstrap.css"));
         }
     }
 }
