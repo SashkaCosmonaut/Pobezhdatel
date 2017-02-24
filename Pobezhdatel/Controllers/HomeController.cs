94d9d41a1c80a657220df65f3a47227975cfddd8
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-02-24 23:44:36 +0300

    Created home page. Old page has been moved to Room controller.

--- Pobezhdatel/Controllers/HomeController.cs
+++ Pobezhdatel/Controllers/HomeController.cs
@@ -1,43 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using log4net;
-using Newtonsoft.Json;
-using Pobezhdatel.Models;
 
 namespace Pobezhdatel.Controllers
 {
     /// <summary>
-    /// Controller for main single page of the application.
+    /// Controller for main home page of the application.
     /// </summary>
     public class HomeController : Controller
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(HomeController));
-
         /// <summary>
-        /// Model for communication with database.
+        /// Show home page.
         /// </summary>
-        protected PobezhdatelDbModel DBModel = new PobezhdatelDbModel();
-
-        /// <summary>
-        /// Show main single page of the application.
-        /// </summary>
-        /// <returns>View of the page.</returns>
+        /// <returns>View of home page.</returns>
         public ActionResult Index()
         {
-            Log.Debug("Index");
-
             return View();
-        }
-
-        /// <summary>
-        /// Get all previous messages that already exist in DB.
-        /// </summary>
-        /// <returns>JSON-string with messages models array.</returns>
-        public string GetMessages()
-        {
-            Log.Debug("GetMessages");
-
-            return JsonConvert.SerializeObject(DBModel.GetMessages());
         }
     }
 }
---
+++ Pobezhdatel/Controllers/RoomController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using log4net;
+using Newtonsoft.Json;
+using Pobezhdatel.Models;
+
+namespace Pobezhdatel.Controllers
+{
+    /// <summary>
+    /// Controller for chat room.
+    /// </summary>
+    public class RoomController : Controller
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(RoomController));
+
+        /// <summary>
+        /// Model for communication with database.
+        /// </summary>
+        protected PobezhdatelDbModel DBModel = new PobezhdatelDbModel();
+
+        /// <summary>
+        /// Show main single page of the application.
+        /// </summary>
+        /// <returns>View of the page.</returns>
+        public ActionResult Index()
+        {
+            Log.Debug("Index");
+
+            return View();
+        }
+
+        /// <summary>
+        /// Get all previous messages that already exist in DB.
+        /// </summary>
+        /// <returns>JSON-string with messages models array.</returns>
+        public string GetMessages()
+        {
+            Log.Debug("GetMessages");
+
+            return JsonConvert.SerializeObject(DBModel.GetMessages());
+        }
+    }
+}
