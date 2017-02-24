ca40b15a5a3d46402336efb932ed21969e42416c
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-02-24 19:46:09 +0300

    Implemented dice rolling.

--- Pobezhdatel/Hubs/ChatHub.cs
+++ Pobezhdatel/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using log4net;
 using Microsoft.AspNet.SignalR;
 using Pobezhdatel.Models;
@@ -26,11 +28,70 @@ namespace Pobezhdatel.Hubs
         {
             Log.Debug("Send");
 
-            var dicesRoll = "qwe";
+            var dicesRoll = DiceRoll(message);
 
             DBModel.AddMessage(new MessageModel(DateTime.Now, playerName, message, dicesRoll));
 
             Clients.All.addMessageToChat(playerName, message, dicesRoll);
         }
+
+        /// <summary>
+        /// Find requests for dices rolling and roll dices.
+        /// </summary>
+        /// <param name="message">Message with possible dice rolling requests.</param>
+        /// <returns>Result of dice rolling or null if there are no requests.</returns>
+        public string DiceRoll(string message)
+        {
+            Log.Debug("DiceRoll");
+
+            try
+            {
+                var matches = Regex.Matches(message, "//[1-9]d(100|[1-9][0-9]?)");
+
+                // Roll every dice roll request and aggreagte all results to one string
+                return matches.Cast<Match>()
+                    .Select(q => GetDiceRollText(q.Value))
+                    .Aggregate((current, next) => current + "\n" + next).TrimEnd('\n');
+            }
+            catch (Exception ex)
+            {
+                Log.Error("", ex);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get text of dice roll in accordance with a requiest.
+        /// Parse dice requests in a message text and append it to text.
+        /// Requiest pattern: //XdY, where X - number of dices, Y - number of dice edges. 
+        /// Number of dices can be 1-9, number of edges can be 1-100 (from 1d1 to 9d100).
+        /// </summary>
+        /// <param name="diceRequest">Text with dice request.</param>
+        protected string GetDiceRollText(string diceRequest)
+        {
+            Log.Debug("GetDiceRollText");
+
+            try
+            {
+                // Parse request
+                var requestParts = diceRequest.TrimStart('/').Split('d');
+                var numberOfDices = int.Parse(requestParts[0]);
+                var numberOfEdges = int.Parse(requestParts[1]);
+
+                var random = new Random(Guid.NewGuid().GetHashCode());
+                var result = diceRequest + ": ";
+
+                for (var i = 0; i < numberOfDices; i++)         // Roll each dice in this request
+                    result += random.Next(1, numberOfEdges) + ", ";
+
+                return result.TrimEnd(' ', ',');
+            }
+            catch (Exception ex)
+            {
+                Log.Error("GetDiceRollText", ex);
+                return "";
+            }
+        }
+
     }
 }
