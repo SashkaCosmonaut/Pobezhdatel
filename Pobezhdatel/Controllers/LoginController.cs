646efbd0b4b7a639d5dc20f3cc1c5bb65ec59b3d
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-04-02 17:58:52 +0300

    Added cookie saving.

--- Pobezhdatel/Controllers/LoginController.cs
+++ Pobezhdatel/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using log4net;
 using Pobezhdatel.Models;
+using System.Web.Security;
+using System.Web;
 
 namespace Pobezhdatel.Controllers
 {
@@ -37,6 +39,20 @@ namespace Pobezhdatel.Controllers
             try
             {
                 if (!ModelState.IsValid) return View("Index", model);
+
+                // Create and save cookies
+                var authTicket = new FormsAuthenticationTicket(
+                    1,
+                    model.PlayerName,                   // Set user name linked with the cookie
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(240),
+                    true,
+                    model.RoomName);                    // Save room name as additional cookie data
+
+                HttpContext.Response.Cookies.Add(
+                    new HttpCookie(
+                        FormsAuthentication.FormsCookieName,
+                        FormsAuthentication.Encrypt(authTicket)));
 
                 return RedirectToAction("Index", "Room");
             }
