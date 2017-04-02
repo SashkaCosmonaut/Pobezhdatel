9518ed72c4cf596268ce0860aa5491ed43af3722
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2017-04-02 22:26:59 +0300

    Implemented displaying the actual state of players in a room.

--- Pobezhdatel/Hubs/ChatHub.cs
+++ Pobezhdatel/Hubs/ChatHub.cs
@@ -31,6 +31,60 @@ namespace Pobezhdatel.Hubs
             new Dictionary<string, Dictionary<string, string>>();
 
         /// <summary>
+        /// Add record about a player in a room.
+        /// </summary>
+        /// <param name="roomName">Name of a room.</param>
+        /// <param name="connectionId">Id of player connection.</param>
+        /// <param name="playerName">Player name.</param>
+        protected static void AddPlayerToRoom(string roomName, string connectionId, string playerName)
+        {
+            Log.Debug("AddPlayerToRoom");
+
+            try
+            {
+                if (!RoomsPlayers.ContainsKey(roomName))
+                {
+                    RoomsPlayers.Add(roomName, new Dictionary<string, string>
+                    {
+                        { connectionId, playerName }
+                    });
+                }
+                else if (!RoomsPlayers[roomName].ContainsKey(connectionId))
+                {
+                    RoomsPlayers[roomName].Add(connectionId, playerName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("AddPlayerToRoom", ex);
+            }
+        }
+
+        /// <summary>
+        /// Delete record about a player in a room.
+        /// </summary>
+        /// <param name="roomName">Name of a room.</param>
+        /// <param name="connectionId">Id of player connection.</param>
+        /// <param name="playerName">Player name.</param>
+        protected static void RemovePlayerFromRoom(string roomName, string connectionId, string playerName)
+        {
+            Log.Debug("RemovePlayerFromRoom");
+
+            try
+            {
+                if (!RoomsPlayers.ContainsKey(roomName)) return;
+
+                RoomsPlayers[roomName].Remove(connectionId);
+
+                if (!RoomsPlayers[roomName].Any()) RoomsPlayers.Remove(roomName);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("RemovePlayerFromRoom", ex);
+            }
+        }
+
+        /// <summary>
         /// Get player data from cookies.
         /// </summary>
         /// <param name="playerName">Name of current player.</param>
@@ -38,19 +92,29 @@ namespace Pobezhdatel.Hubs
         /// <returns></returns>
         private bool GetPlayerData(out string playerName, out string roomName)
         {
+            Log.Debug("GetPlayerData");
+
             playerName = "";
             roomName = "";
 
-            var authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            try
+            {
+                var authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
 
-            if (authCookie?.Value == null) return false;
+                if (authCookie?.Value == null) return false;
 
-            var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
 
-            playerName = authTicket.Name;
-            roomName = authTicket.UserData;
+                playerName = authTicket.Name;
+                roomName = authTicket.UserData;
 
-            return true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("RemovePlayerFromRoom", ex);
+                return false;
+            }
         }
 
         /// <summary>
@@ -59,17 +123,28 @@ namespace Pobezhdatel.Hubs
         /// <returns>Task for async call of this method.</returns>
         public override Task OnConnected()
         {
-            var playerName = "";
-            var roomName = "";
+            Log.Debug("OnConnected");
 
-            if (GetPlayerData(out playerName, out roomName))                // If player data is successfully taken from cookies
+            try
             {
-                Groups.Add(Context.ConnectionId, roomName).Wait();          // Add player to group and wait for it
+                var playerName = "";
+                var roomName = "";
 
-                // Take players of this room, convert them to array of objects for client and send to it
-                Clients.Group(roomName).playerJoinRoom(
-                    JsonConvert.SerializeObject(
-                        RoomsPlayers[roomName].Select(q => new { id = q.Key, name = q.Value})), playerName, roomName);
+                if (GetPlayerData(out playerName, out roomName))                // If player data is successfully taken from cookies
+                {
+                    Groups.Add(Context.ConnectionId, roomName).Wait();          // Add player to group and wait for it
+
+                    AddPlayerToRoom(roomName, Context.ConnectionId, playerName);
+
+                    // Take players of this room, convert them to array of objects for client and send to it
+                    Clients.Group(roomName).playerJoinRoom(
+                        JsonConvert.SerializeObject(
+                            RoomsPlayers[roomName].Select(q => new { id = q.Key, name = q.Value })), playerName, roomName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("OnConnected", ex);
             }
 
             return base.OnConnected();
@@ -82,12 +157,25 @@ namespace Pobezhdatel.Hubs
         /// <returns>Task for async call of this method.</returns>
         public override Task OnDisconnected(bool stopCalled)
         {
-            var playerName = "";
-            var roomName = "";
+            Log.Debug("OnDisconnected");
 
-            if (GetPlayerData(out playerName, out roomName))                    // If player data is successfully taken from cookies
-                Clients.Group(roomName).playerLeaveRoom(Context.ConnectionId);  // Notify other players in this room
-            
+            try
+            {
+                var playerName = "";
+                var roomName = "";
+
+                if (GetPlayerData(out playerName, out roomName))                    // If player data is successfully taken from cookies
+                {
+                    RemovePlayerFromRoom(roomName, Context.ConnectionId, playerName);
+
+                    Clients.Group(roomName).playerLeaveRoom(Context.ConnectionId);  // Notify other players in this room
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("OnDisconnected", ex);
+            }
+
             return base.OnDisconnected(stopCalled);
         }
 
@@ -97,6 +185,8 @@ namespace Pobezhdatel.Hubs
         /// <returns>Task for async call of this method.</returns>
         public override Task OnReconnected()
         {
+            Log.Debug("OnReconnected");
+
             return base.OnReconnected();
         }
 
@@ -108,13 +198,20 @@ namespace Pobezhdatel.Hubs
         {
             Log.Debug("Send");
 
-            var dicesRoll = Utilities.DiceRoll(message);
-
-            // If message succesfully sent to DB, show it in the chat
-            if (DBModel.AddMessage(new MessageModel(DateTime.Now, Clients.Caller.roomName, Clients.Caller.playerName, message, dicesRoll)))
+            try
             {
-                // We must pass a string to this method, not dynamic type
-                Clients.Group((string)Clients.Caller.roomName).addMessageToChat(message, dicesRoll);
+                var dicesRoll = Utilities.DiceRoll(message);
+
+                // If message succesfully sent to DB, show it in the chat
+                if (DBModel.AddMessage(new MessageModel(DateTime.Now, Clients.Caller.roomName, Clients.Caller.playerName, message, dicesRoll)))
+                {
+                    // We must pass a string to this method, not dynamic type
+                    Clients.Group((string)Clients.Caller.roomName).addMessageToChat(message, dicesRoll);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Send", ex);
             }
         }
     }
